e9b7662b406b6e8638b9a5d78effe53823dad2c1
Bin Yang <bin.yang@amlogic.com>
2019-12-17 10:53:58 +0800

    Build TA using Android make rules: libmediadrm
    Using checkbuild is not possible as long as the build system is trying
    to subshell out to `make`. Everything needs to be in the Android build
    system.
    
    Test: make $OUT/vendor/lib/teetz/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta (hdcp)
    Test: md5sum = c189ef2bf2b0adac242cecf986c83786
    
    Test: make $OUT/vendor/lib/teetz/af1ae3a4-888b-4e11-b4ab1c2b972d1c11.ta (netflix)
    Test: md5sum = 9d0903554aa00138eb3a124a2c97d095
    
    Test: make $OUT/vendor/lib/teetz/2c1a33c0-44cc-11e5-bc3b0002a5d5c51b.ta (secmem)
    Test: md5sum = 25f40ce9f77d6609493270317bf01137
    
    Test: make $OUT/vendor/lib/teetz/e043cde0-61d0-11e5-9c260002a5d5c51b.ta (wvcenc)
    Test: md5sum = ba2cd7deed097451059e6bf9549318f1
    
    Test: make $OUT/vendor/lib/teetz/9a04f079-9840-4286-ab92e65be0885f95.ta (playready)
    Test: md5sum = 048d4ff9c1e605bfc6c568906519ef47
    
    Test: make $OUT/vendor/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta (videofirm)
    Test: md5sum = 671e27062333d87c3d209c3b95fbbed0
    
    Bug: 144863396
    Test: lunch adt3-userdebug; make checkbuild
    
    Change-Id: I44ec81ee770572185641d6a93a87092014c2f6cb
    Signed-off-by: Bin Yang <bin.yang@amlogic.com>

--- Android.mk
+++ Android.mk
@@ -48,26 +48,29 @@ include $(BUILD_PREBUILT)
 #####################################################################
 include $(CLEAR_VARS)
 LOCAL_MODULE := 9a04f079-9840-4286-ab92e65be0885f95
-ifeq ($(TARGET_ENABLE_TA_SIGN), true)
-$(info $(shell mkdir -p $(shell pwd)/$(PRODUCT_OUT)/signed/mediadrm/TVP))
-$(info $(shell $(shell pwd)/$(BOARD_AML_VENDOR_PATH)/tdk/ta_export/scripts/sign_ta_auto.py \
-		--in=$(shell pwd)/$(LOCAL_PATH)/mediadrm/TVP/9a04f079-9840-4286-ab92e65be0885f95.ta \
-		--out=$(shell pwd)/$(PRODUCT_OUT)/signed/mediadrm/TVP/9a04f079-9840-4286-ab92e65be0885f95.ta \
-		--keydir=$(shell pwd)/$(BOARD_AML_TDK_KEY_PATH)))
-LOCAL_SRC_FILES := ../../../../../../$(PRODUCT_OUT)/signed/mediadrm/TVP/9a04f079-9840-4286-ab92e65be0885f95.ta
-else
-LOCAL_SRC_FILES := mediadrm/TVP/9a04f079-9840-4286-ab92e65be0885f95.ta
-endif
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/teetz
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .ta
+LOCAL_MODULE_TAGS := optional
 LOCAL_32_BIT_ONLY := true
-
 LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/teetz
-
 LOCAL_STRIP_MODULE := false
+
+PLAYREADY_UNSIGNED_TA := $(LOCAL_PATH)/mediadrm/TVP/9a04f079-9840-4286-ab92e65be0885f95.ta
+
+ifeq ($(TARGET_ENABLE_TA_SIGN), true)
+include $(BUILD_SYSTEM)/base_rules.mk
+$(LOCAL_BUILT_MODULE): $(PLAYREADY_UNSIGNED_TA)
+	@mkdir -p $(dir $@)
+	$(BOARD_AML_VENDOR_PATH)/tdk/ta_export/scripts/sign_ta_auto.py \
+		--in=$(PLAYREADY_UNSIGNED_TA) \
+		--out=$@ \
+		--keydir=$(BOARD_AML_TDK_KEY_PATH)
+else
+LOCAL_PREBUILT_MODULE_FILE := $(PLAYREADY_UNSIGNED_TA)
 include $(BUILD_PREBUILT)
+endif
+
 #####################################################################
 else
 
